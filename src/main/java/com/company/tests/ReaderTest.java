90f51fe5786e24fb594c15ea663fb16c9ef25ac8
AnnaSherepa <annasherepa2@gmail.com>
2020-11-24 14:44:31 +0200

    LabWork2

--- src/main/java/com/company/tests/ReaderTest.java
+++ src/main/java/com/company/tests/ReaderTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 public class ReaderTest {
 
     @Test
-    public void setRecord_Set_One_Record() {
+    public void setRecordSetOneRecord() {
         Reader r = new Reader("Анна", "Шерепа", "Валеріївна", 19, Sex.woman, Type.all);
         Librarian librarian = new Librarian("Олександр", "Мельник", "Іванович", 46, Sex.man, "+067-063-32-21", 456.2);
         Author a = new Author("Тарас", "Шевченко","Григорович", 47, Sex.man, 1840, 1861);
@@ -16,7 +16,7 @@ public class ReaderTest {
     }
 
     @Test
-    public void setRecord_Set_More_That_One() {
+    public void setRecordSetMoreThatOne() {
         Reader r = new Reader("Анна", "Шерепа", "Валеріївна", 19, Sex.woman, Type.all);
         Librarian librarian = new Librarian("Олександр", "Мельник", "Іванович", 46, Sex.man, "+067-063-32-21", 456.2);
 
@@ -30,7 +30,7 @@ public class ReaderTest {
 
 
     @Test
-    public void setRecord_Check_An_Access() {
+    public void setRecordCheckAnAccess() {
         Reader r = new Reader("Анна", "Шерепа", "Валеріївна", 19, Sex.woman, Type.all);
         Librarian librarian = new Librarian("Олександр", "Мельник", "Іванович", 46, Sex.man, "+067-063-32-21", 456.2);
         Author a = new Author("Тарас", "Шевченко","Григорович", 47, Sex.man, 1840, 1861);
@@ -40,7 +40,7 @@ public class ReaderTest {
     }
 
     @Test
-    public void deleteRecord_From_Reader_List_Of_Record() {
+    public void deleteRecordFromReaderListOfRecord() {
         Reader r = new Reader("Анна", "Шерепа", "Валеріївна", 19, Sex.woman, Type.all);
         Librarian librarian = new Librarian("Олександр", "Мельник", "Іванович", 46, Sex.man, "+067-063-32-21", 456.2);
 
@@ -52,13 +52,13 @@ public class ReaderTest {
     }
 
     @Test
-    public void deleteRecord_Not_Existed_Record_null() {
+    public void deleteRecordNotExistedRecordNull() {
         Reader r = new Reader("Анна", "Шерепа", "Валеріївна", 19, Sex.woman, Type.all);
         r.deleteRecord(0);
     }
 
     @Test
-    public void searchRecordId_Return_Id() {
+    public void searchRecordIdReturnId() {
         Reader r = new Reader("Анна", "Шерепа", "Валеріївна", 19, Sex.woman, Type.all);
         Librarian librarian = new Librarian("Олександр", "Мельник", "Іванович", 46, Sex.man, "+067-063-32-21", 456.2);
         Author a = new Author("Тарас", "Шевченко","Григорович", 47, Sex.man, 1840, 1861);
